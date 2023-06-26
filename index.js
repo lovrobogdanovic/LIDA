2890c4daa663c9142d77715e7dfe18fa9a4eeda2
Ana Puhek <ana@insa-software.com>
2023-06-26 20:28:00 +0200

    commit ana

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ var carousel = new bootstrap.Carousel(myCarousel, {
 //news
 
 // < script src = "https://code.jquery.com/jquery-3.6.0.min.js" ></script>
-// <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+//<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
 
 $(document).ready(function () {
     // Onemogućava automatsko listanje karusela
